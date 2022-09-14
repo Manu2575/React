5f84ac1437f2dbdb89a9bf9f41622e4123cff2e8
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-09-14 19:47:09 -0300

    Projeto Lista de Tarefas utilizando TypeScript

---
+++ todo/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
+
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão na padaria', done: false },
+    { id: 2, name: 'Comprar um bolo na padaria', done: true },
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header> Lista de Tarefas </C.Header>
+
+        <AddArea onEnter={handleAddTask} />
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item}/>
+        ))}
+
+      </C.Area>
+    </C.Container>
+  );
+}
+export default App;
